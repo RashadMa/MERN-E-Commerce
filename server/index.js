7343a5af86c6171916a1a968295a09cecba02060
rashadma <rashadkhm@code.edu.az>
2024-02-01 20:18:19 +0200

    acess control updated

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ connectDB()
 const app = express()
 
 app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+      res.header('Access-Control-Allow-Origin', 'https://mern-store-6dt0.onrender.com');
       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
       res.header('Access-Control-Allow-Headers', 'Content-Type');
       next();
