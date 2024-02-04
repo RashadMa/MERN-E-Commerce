982d2ff2eeda3bf9281ae55773de4cbbd0223ba9
rashadma <rashadkhm@code.edu.az>
2024-02-04 14:31:29 +0200

    create token edi -2

--- server/utils/createToken.js
+++ server/utils/createToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, userId) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "strict",
-            maxAge: 30 * 24 * 60 * 60 * 1000,
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       })
       return token
 }
