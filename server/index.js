2ca10c0f0385aa4f9d7d878a80036496f1f4c5b8
rashadma <rashadkhm@code.edu.az>
2024-02-05 15:35:07 +0200

    edit

--- server/index.js
+++ server/index.js
@@ -17,12 +17,12 @@ const app = express()
 
 app.use(cors());
 
-// app.use((req, res, next) => {
-//       res.header('Access-Control-Allow-Origin', '*');
-//       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//       res.header('Access-Control-Allow-Headers', 'Content-Type');
-//       next();
-// });
+app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+});
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
