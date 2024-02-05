632516c30c910b614dc23d504e07bae5f18ab345
rashadma <rashadkhm@code.edu.az>
2024-02-05 15:08:40 +0200

    cors edited

--- server/index.js
+++ server/index.js
@@ -16,17 +16,17 @@ connectDB()
 const app = express()
 
 app.use(cors({
-      origin: 'https://mern-e-commerce-api-2.vercel.app',
+      origin: '*',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
 }));
 
-app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type');
-      next();
-});
+// app.use((req, res, next) => {
+//       res.header('Access-Control-Allow-Origin', '*');
+//       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//       res.header('Access-Control-Allow-Headers', 'Content-Type');
+//       next();
+// });
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
