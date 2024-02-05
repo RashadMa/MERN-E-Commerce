70254030dabd2e2a96732e58ff56d19d48b6c705
rashadma <rashadkhm@code.edu.az>
2024-02-05 14:40:21 +0200

    Test

--- server/index.js
+++ server/index.js
@@ -21,10 +21,14 @@ app.use((req, res, next) => {
       next();
 });
 
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-
+app.use("/", (req, res) => {
+      res.send("salam")
+})
 app.use("/api/users", userRoute)
 app.use('/api/category', categoryRoutes)
 app.use('/api/products', productRoutes)
